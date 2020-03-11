8678efc88078eb5817edad4531a210959b5cd505
Peerchemist <dx.1178@gmail.com>
2020-03-11 17:04:19 +0100

    ran black formatter against conf.py

--- src/conf.py
+++ src/conf.py
@@ -3,20 +3,20 @@ from os import environ
 BOT_VERSION = "0.1"
 DEPOSIT_CHECK_JOB = 60.0  # seconds
 
-if environ.get('APP_ENV') == 'docker':
-    RPC_PORT = environ.get('RPC_PORT', '9904')
-    RPC_USER = environ.get('RPC_USER', 'rpc')
-    RPC_PASSWORD = environ.get('RPC_PASSWORD', 'pass')
-    RPC_HOST = environ.get('RPC_HOST', '')
-    BOT_ID = environ.get('BOT_ID', '')
-    BOT_TOKEN = environ.get('BOT_TOKEN', '')
-    DB_NAME = environ.get('DB_NAME', 'tippbot.db')
+if environ.get("APP_ENV") == "docker":
+    RPC_PORT = environ.get("RPC_PORT", "9904")
+    RPC_USER = environ.get("RPC_USER", "rpc")
+    RPC_PASSWORD = environ.get("RPC_PASSWORD", "pass")
+    RPC_HOST = environ.get("RPC_HOST", "")
+    BOT_ID = environ.get("BOT_ID", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    DB_NAME = environ.get("DB_NAME", "tippbot.db")
 
 else:
-    RPC_PORT = environ.get('RPC_PORT', '9904')
-    RPC_USER = environ.get('RPC_USER', 'rpc')
-    RPC_PASSWORD = environ.get('RPC_PASSWORD', 'pass')
-    RPC_HOST = environ.get('RPC_HOST', '127.0.0.1')
-    BOT_ID = environ.get('BOT_ID', '')
-    BOT_TOKEN = environ.get('BOT_TOKEN', '')
-    DB_NAME = environ.get('DB_NAME', 'tippbot.db')
+    RPC_PORT = environ.get("RPC_PORT", "9904")
+    RPC_USER = environ.get("RPC_USER", "rpc")
+    RPC_PASSWORD = environ.get("RPC_PASSWORD", "pass")
+    RPC_HOST = environ.get("RPC_HOST", "")
+    BOT_ID = environ.get("BOT_ID", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    DB_NAME = environ.get("DB_NAME", "tippbot.db")
