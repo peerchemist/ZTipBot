11fa015ba666396ad4ee02f6a46e7f0d49a6a16e
Peerchemist <dx.1178@gmail.com>
2020-03-11 13:05:53 +0100

    conf logic

--- src/conf.py
+++ src/conf.py
@@ -5,10 +5,16 @@ DEPOSIT_CHECK_JOB = 60.0  # seconds
 
 if environ.get('APP_ENV') == 'docker':
     RPC_PORT = environ.get('RPC_PORT', '9904')
-    RPC_USER = environ.get('RPC_USER', 'user')
+    RPC_USER = environ.get('RPC_USER', 'rpc')
     RPC_PASSWORD = environ.get('RPC_PASSWORD', 'pass')
+    BOT_ID = environ.get('BOT_ID', '')
+    BOT_TOKEN = environ.get('BOT_TOKEN', '')
+    DB_NAME = environ.get('DB_NAME', 'tippbot.db')
 
 else:
     RPC_PORT = environ.get('RPC_PORT', '9904')
-    RPC_USER = environ.get('RPC_USER', 'user')
+    RPC_USER = environ.get('RPC_USER', 'rpc')
     RPC_PASSWORD = environ.get('RPC_PASSWORD', 'pass')
+    BOT_ID = environ.get('BOT_ID', '')
+    BOT_TOKEN = environ.get('BOT_TOKEN', '')
+    DB_NAME = environ.get('DB_NAME', 'tippbot.db')
