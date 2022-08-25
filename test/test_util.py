7146b3a33588637462b54320d6c5e58ecc8c8f99
Peerchemist <dx.1178@gmail.com>
2022-08-25 15:59:52 +0200

    test for find_address

--- test/test_util.py
+++ test/test_util.py
@@ -13,3 +13,8 @@ def test_find_amount():
     with pytest.raises(util.TipBotException, match="too_many_decimals"):
         util.find_amount("$tip @belovachap 10.0000000 ppc")
 
+
+def test_find_address():
+
+    assert util.find_address("$tip FouNdAtiOn 10 ppc") == "p92W3t7YkKfQEPDb7cG9jQ6iMh7cpKLvwK"
+    assert util.find_address("$withdraw PBGM16nDjX4x8NXrdKHDk1gsAndVHzh3fv 10") == "PBGM16nDjX4x8NXrdKHDk1gsAndVHzh3fv"
